3423118ed5514ab02521e790d1c06f5ae40c536a
yisak Alemayehu <yisakalemayehuaga@gmail.com>
2026-03-12 10:12:53 +0300

    k

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>messaging app</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body class= "bg-gray-100">
+    <!-- flex items-center justify-center min-h-screen -->
+    <!--
+        Container div for the main content of the Messenger app.
+        - Uses Tailwind CSS classes:
+          - `container`: Centers and constrains content width.
+          - `mx-auto`: Horizontally centers the container.
+          - `p-4`: Adds padding around the content.
+    -->
+    <div class="container mx-auto p-4">
+        <!-- Displays the main heading "Messaging App" with styling for large, bold text and margin below. -->
+        <h1 class ="text-2xl font-bold mb-4">Messaging App</h1>
+
+    </div>
+    
+</body>
+</html>
